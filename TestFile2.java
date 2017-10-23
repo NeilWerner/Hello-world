e9777df835fdd2235f0d82094198a7f143d62496
NeilWerner <32942563+NeilWerner@users.noreply.github.com>
2017-10-23 12:02:05 +0200

    Create TestFile2.java
    Our test file 2 for test github

---
+++ TestFile2.java
@@ -0,0 +1,9 @@
+This is a second file to test how github woks. For the moment I won't create a new branch for the instant, we will work in the master branch.
+
+That's all for the moment.
+
+Let me your suggestions or comments.
+
+Have a nice day,
+
+Werner
